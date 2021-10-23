0a397ef7358b0640d59112b3de688f18f991553e
hta <agathamagnusg@hotmail.com>
2021-10-23 14:41:24 -0300

    IMC

---
+++ IMC/index.py
@@ -0,0 +1,20 @@
+"""Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
+– IMC abaixo de 18,5: Abaixo do Peso
+– Entre 18,5 e 25: Peso Ideal
+– 25 até 30: Sobrepeso
+– 30 até 40: Obesidade
+– Acima de 40: Obesidade Mórbida"""
+peso = float(input('Qual é seu peso? (Kg) '))
+altura = float(input('Qual é a sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está ABAIXO do peso normal.')
+elif 18.5 <= imc < 25:
+    print('PARABÉNS, você está na faixa de peso normal')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDADE, CUIDADO!')
+elif imc >= 40:
+    print('Você está em OBESIDADE MÓRBIDA, CUIDADO!')
